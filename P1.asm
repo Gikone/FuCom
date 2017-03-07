abcd8785227dbd434b8fa44e390382868ebb1271
Gikone <diegonzalez_96@hotmail.com>
2017-03-07 19:53:31 +0100

    Create P1.asm

---
+++ P1.asm
@@ -0,0 +1,53 @@
+; ---------------------------------
+;  AUTHOR: Díaz García, Rodrigo
+;  DATE: 22 February 2017
+;  ASSIGNMENT: P1
+;  REVISION: 1.2 - Corrected date
+; ---------------------------------
+.ORG 100AH
+; ------------
+;  Ejercicio 1
+; ------------
+MVI A, 1H
+MVI B, 2H
+MVI C, 3H
+MVI D, 4H
+MVI E, 5H
+MVI H, 6H
+MVI L, 7H
+; ------------
+;  Ejercicio 2
+; ------------
+MOV M, A
+; ------------
+;  Ejercicio 3
+; ------------
+INX H
+MOV M, B
+INX H
+MOV M, C
+INX H
+MOV M, D
+INX H
+MOV M, E
+; ------------
+;  Ejercicio 4
+; ------------
+SHLD 60FH
+; ------------
+;  Ejercicio 5
+; ------------
+XCHG
+MOV M, A
+; ------------
+;  Ejercicio 6
+; ------------
+LHLD 60FH
+MOV M, C
+; ------------
+;  Ejercicio 7
+; ------------
+STAX B
+MVI A, 44H
+STAX D
+HLT 
