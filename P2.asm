eedaaaad494510834e7d12335222d6453db1ca42
Gikone <diegonzalez_96@hotmail.com>
2017-03-07 19:55:44 +0100

    Create P2.asm

---
+++ P2.asm
@@ -0,0 +1,19 @@
+; ---------------------------------
+;  AUTHOR: D.G.Romano
+;  DATE: 22 February 2017
+;  ASSIGNMENT: P2
+;  REVISION: 1.0
+; ---------------------------------
+
+.DATA 0071H ; Compiler Directive
+  dB 1,2,3,4,5 ; Compiler Directive
+.ORG 100AH ; Compiler Directive
+
+LXI H, 71H ; Initialize H Pair 
+MVI B, 4 ; Initizalize B to (n-1)
+REP: ADD M ; Loop Marker: Increase A by M
+INX H ; Increase memory address by 1
+DCR B ; Decrease B by 1
+JP REP ; If B is positive go to REP
+MOV M, A ; Save to memory
+HLT ; End
