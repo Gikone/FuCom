9c2d5775f6912c9b9a9f1de9793adafd8ae4fe98
Gikone <diegonzalez_96@hotmail.com>
2017-06-14 20:53:18 +0300

    Add files via upload

--- ExFC102.asm
+++ ExFC102.asm
@@ -5,37 +5,27 @@
 ;Enunciado: Rutina en el simulador que cuando pulses un numero se represente en la pantalla, se repite hasta que pulses el 0 y el programa termine. Interrupcion 5.5
 
 .ORG 1200H
-EI ;interrupciones
+
 MVI A, 08H ; Inicializa A, a mascara de interrupciones
 MVI B, FFH ; Inicializa B
 MVI C, FFH ; Inicializa C
 LXI SP, 1200H ;pila
-Bucle:
-       RIM ;lectura mascara
-	 ;ANI 0010000B ;and
-	 CPI 3AH
-	 RNC
-	 CPI 30H
-	 ;JZ Final ; si 0 final
-	; JC Bucle 
-	 ;JNZ Bucle ; si no es 0 bucle
-	 ;JMP Bucle ; si 0 vuelve a bucle
-	 ;CALL 0H ;rutina 044EH
-;MVI E, 0H ;cargar E
-;CMP E ;comparacion
+REP: 
+	EI
+	RIM
+	CALL 2CH 
+	JMP REP
 
-CALL 2Ch
 
 ;Teclado
-.ORG 2Ch ; Interrupción 5.5
+.ORG 2CH ; Interrupción 5.5
 IN 00
+CPI 3AH
+SUI 30H
 CPI 30H
-RNC
-;SUI 30H
+HLT
 
-Final:
-	; RST 1
-	 HLT
+
 
 
 
