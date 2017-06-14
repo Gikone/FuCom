7ec19a0399704ec14d90f647faa130955d7cadfc
Gikone <diegonzalez_96@hotmail.com>
2017-06-14 20:34:52 +0300

    Add files via upload

--- ExFC102.asm
+++ ExFC102.asm
@@ -6,27 +6,32 @@
 
 .ORG 1200H
 EI ;interrupciones
+MVI A, 08H ; Inicializa A, a mascara de interrupciones
+MVI B, FFH ; Inicializa B
+MVI C, FFH ; Inicializa C
 LXI SP, 1200H ;pila
-MVI A,00001000B ;mascara al acumulador
-MOV B, A ;acumulador a B
-CALL 2Ch
-
-;Teclado
-.ORG 2Ch ; Interrupción 5.5
-IN 00
 Bucle:
        RIM ;lectura mascara
-	 ANI 0010000B ;and
+	 ;ANI 0010000B ;and
 	 CPI 3AH
 	 RNC
 	 CPI 30H
-	 JZ Final ; si 0 final
+	 ;JZ Final ; si 0 final
 	 JC Bucle 
 	 ;JNZ Bucle ; si no es 0 bucle
 	 ;JMP Bucle ; si 0 vuelve a bucle
 	 ;CALL 0H ;rutina 044EH
 ;MVI E, 0H ;cargar E
 ;CMP E ;comparacion
+
+CALL 2Ch
+
+;Teclado
+.ORG 2Ch ; Interrupción 5.5
+IN 00
+CPI 30H
+RNC
+;SUI 30H
 
 Final:
 	; RST 1
