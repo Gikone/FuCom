94f98df9be778acb53a84237c53c8e5576bff800
Gikone <diegonzalez_96@hotmail.com>
2017-06-14 20:16:53 +0300

    Add files via upload

--- ExFC102.asm
+++ ExFC102.asm
@@ -14,22 +14,24 @@ CALL 2Ch
 ;Teclado
 .ORG 2Ch ; Interrupción 5.5
 IN 00
-MOV B,A
-IN 00H
-MOV C,A
+
 
 Bucle:
        RIM ;lectura mascara
 	 ANI 0010000B ;and
-	 JNZ Bucle ; si no es 0 bucle
+	 CPI 3AH
+	 RNC
+	 CPI 30H
+	 JZ Final ; si 0 final
+	 JC Bucle 
+	 ;JNZ Bucle ; si no es 0 bucle
 	 ;JMP Bucle ; si 0 vuelve a bucle
 	 ;CALL 0H ;rutina 044EH
-MVI E, 0H ;cargar E
-CMP E ;comparacion
-;JZ Final ; si 0 final
+;MVI E, 0H ;cargar E
+;CMP E ;comparacion
 
 Final:
-	 RST 1
+	; RST 1
 	 HLT
 
 
